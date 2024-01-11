988fa3fc45306428e77644741732853d132080a8
Dharshini-S12 <118833017+Dharshini-S12@users.noreply.github.com>
2024-01-11 22:23:24 +0530

    body commit

--- swiggy-clone/src/components/Body.js
+++ swiggy-clone/src/components/Body.js
@@ -23,73 +23,75 @@ function Body() {
           <a className='body-button-a' href=''>Less then Rs.300</a>
        </div>
       <div className='body-image-container-main'>
-      <div className='body-image-container'>
-        <a href='' className='body-image-container-anchor'>
-          <div class="body-image-container-1">
-            <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/47eabee7009164ca18a3ccf1dcc17377" ></img>
-            <div class="centered">10% OFF UPTO ₹100</div>
+          <div className='body-image-container'>
+              <a href='' className='body-image-container-anchor'>
+              <div class="body-image-container-1">
+                  <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/47eabee7009164ca18a3ccf1dcc17377" ></img>
+                  <div class="centered">10% OFF UPTO ₹100</div>
+              </div>
+              <div className='body-image-container-2'>
+                  <span className='body-image-container-text'>Hotel Aryaas</span>
+                  <div className='body-image-container-3'>
+                      <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                      <span className='body-image-container-text-1' >4.5 &#8226; 14mins</span><br></br>
+                  </div>
+                  <span className='body-image-container-text-2' >South Indian<br></br>Pollachi</span>
+              </div>
+              </a>
           </div>
-          <div className='body-image-container-2'>
-           <span className='body-image-container-text'>Hotel Aryaas</span>
-           <div className='body-image-container-3'>
-            <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
-            <span className='body-image-container-text-1' >4.5 &#8226; 14mins</span><br></br>
-           </div>
-           <span className='body-image-container-text-2' >South Indian<br></br>Pollachi</span>
-          </div>
-        </a>
-      </div>
-      <div className='body-image-container'>
-        <a href='' className='body-image-container-anchor'>
-          <div class="body-image-container-1">
-            <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/f1bc9ddf53de574cdc35ab2f717df234" ></img>
+          <div className='body-image-container'>
+            <a href='' className='body-image-container-anchor'>
+            <div class="body-image-container-1">
+              <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/f1bc9ddf53de574cdc35ab2f717df234" ></img>
           
+            </div>
+            <div className='body-image-container-2'>
+              <span className='body-image-container-text'>Hotel Gowri KKrishna</span>
+              <div className='body-image-container-3'>
+                <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                <span className='body-image-container-text-1' >4.3 &#8226; 18mins</span><br></br>
+              </div>
+              <span className='body-image-container-text-2' >South Indian,North Indian,..<br></br>Pollachi</span>
+            </div>
+            </a>
           </div>
-          <div className='body-image-container-2'>
-           <span className='body-image-container-text'>Hotel Gowri KKrishna</span>
-           <div className='body-image-container-3'>
-            <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
-            <span className='body-image-container-text-1' >4.3 &#8226; 18mins</span><br></br>
-           </div>
-           <span className='body-image-container-text-2' >South Indian,North Indian,..<br></br>Pollachi</span>
+          <div className='body-image-container'>
+            <a href='' className='body-image-container-anchor'>
+            <div class="body-image-container-1">
+              <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b14cd9fc40129fcfb97aa7e621719d07" ></img>
+              <div class="centered">10% OFF UPTO ₹40</div>
+            </div>
+            <div className='body-image-container-2'>
+              <span className='body-image-container-text'>Madhava Multicusine...</span>
+              <div className='body-image-container-3'>
+                <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                <span className='body-image-container-text-1' >4.6 &#8226; 18mins</span><br></br>
+              </div>
+              <span className='body-image-container-text-2' >South Indian,North Indian,..<br></br>Pollachi</span>
+            </div>
+            </a>
           </div>
-        </a>
-      </div>
-      <div className='body-image-container'>
-        <a href='' className='body-image-container-anchor'>
-          <div class="body-image-container-1">
-            <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b14cd9fc40129fcfb97aa7e621719d07" ></img>
-            <div class="centered">10% OFF UPTO ₹40</div>
+          <div className='body-image-container'>
+            <a href='' className='body-image-container-anchor'>
+              <div class="body-image-container-1">
+                <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uorbn09yih2szvmzvtql" ></img>
+                <div class="centered">50% OFF UPTO ₹100</div>
+              </div>
+              <div className='body-image-container-2'>
+                <span className='body-image-container-text'>Kwality Walls Frozen Dess..</span>
+                <div className='body-image-container-3'>
+                  <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                  <span className='body-image-container-text-1' >4.6 &#8226; 16mins</span><br></br>
+                </div>
+                <span className='body-image-container-text-2' >Desserts, Ice Cream, Ice Cream...<br></br>Pollachi</span>
+              </div>
+            </a>
           </div>
-          <div className='body-image-container-2'>
-           <span className='body-image-container-text'>Madhava Multicusine...</span>
-           <div className='body-image-container-3'>
-            <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
-            <span className='body-image-container-text-1' >4.6 &#8226; 18mins</span><br></br>
-           </div>
-           <span className='body-image-container-text-2' >South Indian,North Indian,..<br></br>Pollachi</span>
-          </div>
-        </a>
-      </div>
-      <div className='body-image-container'>
-        <a href='' className='body-image-container-anchor'>
-          <div class="body-image-container-1">
-            <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uorbn09yih2szvmzvtql" ></img>
-            <div class="centered">50% OFF UPTO ₹100</div>
-          </div>
-          <div className='body-image-container-2'>
-           <span className='body-image-container-text'>Kwality Walls Frozen Dess..</span>
-           <div className='body-image-container-3'>
-            <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
-            <span className='body-image-container-text-1' >4.6 &#8226; 16mins</span><br></br>
-           </div>
-           <span className='body-image-container-text-2' >Desserts, Ice Cream, Ice Cream...<br></br>Pollachi</span>
-          </div>
-        </a>
       </div>
 
 
-      </div>
+
+
       <div className='body-image-container-main'>
       <div className='body-image-container'>
         <a href='' className='body-image-container-anchor'>
@@ -155,6 +157,151 @@ function Body() {
         </a>
       </div>
       </div>
+
+
+
+
+      <div className='body-image-container-main'>
+          <div className='body-image-container'>
+              <a href='' className='body-image-container-anchor'>
+              <div class="body-image-container-1">
+                  <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/47eabee7009164ca18a3ccf1dcc17377" ></img>
+                  <div class="centered">10% OFF UPTO ₹100</div>
+              </div>
+              <div className='body-image-container-2'>
+                  <span className='body-image-container-text'>Hotel Aryaas</span>
+                  <div className='body-image-container-3'>
+                      <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                      <span className='body-image-container-text-1' >4.5 &#8226; 14mins</span><br></br>
+                  </div>
+                  <span className='body-image-container-text-2' >South Indian<br></br>Pollachi</span>
+              </div>
+              </a>
+          </div>
+        <div className='body-image-container'>
+            <a href='' className='body-image-container-anchor'>
+            <div class="body-image-container-1">
+              <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/f1bc9ddf53de574cdc35ab2f717df234" ></img>
+          
+            </div>
+            <div className='body-image-container-2'>
+              <span className='body-image-container-text'>Hotel Gowri KKrishna</span>
+              <div className='body-image-container-3'>
+                <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                <span className='body-image-container-text-1' >4.3 &#8226; 18mins</span><br></br>
+              </div>
+              <span className='body-image-container-text-2' >South Indian,North Indian,..<br></br>Pollachi</span>
+            </div>
+            </a>
+        </div>
+        <div className='body-image-container'>
+            <a href='' className='body-image-container-anchor'>
+            <div class="body-image-container-1">
+              <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b14cd9fc40129fcfb97aa7e621719d07" ></img>
+              <div class="centered">10% OFF UPTO ₹40</div>
+            </div>
+            <div className='body-image-container-2'>
+              <span className='body-image-container-text'>Madhava Multicusine...</span>
+              <div className='body-image-container-3'>
+                <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                <span className='body-image-container-text-1' >4.6 &#8226; 18mins</span><br></br>
+              </div>
+              <span className='body-image-container-text-2' >South Indian,North Indian,..<br></br>Pollachi</span>
+            </div>
+            </a>
+        </div>
+        <div className='body-image-container'>
+            <a href='' className='body-image-container-anchor'>
+              <div class="body-image-container-1">
+                <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uorbn09yih2szvmzvtql" ></img>
+                <div class="centered">50% OFF UPTO ₹100</div>
+              </div>
+              <div className='body-image-container-2'>
+                <span className='body-image-container-text'>Kwality Walls Frozen Dess..</span>
+                <div className='body-image-container-3'>
+                  <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                  <span className='body-image-container-text-1' >4.6 &#8226; 16mins</span><br></br>
+                </div>
+                <span className='body-image-container-text-2' >Desserts, Ice Cream, Ice Cream...<br></br>Pollachi</span>
+              </div>
+            </a>
+        </div>
+      </div>
+
+
+
+
+
+
+
+      <div className='body-image-container-main'>
+          <div className='body-image-container'>
+              <a href='' className='body-image-container-anchor'>
+              <div class="body-image-container-1">
+                  <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/47eabee7009164ca18a3ccf1dcc17377" ></img>
+                  <div class="centered">10% OFF UPTO ₹100</div>
+              </div>
+              <div className='body-image-container-2'>
+                  <span className='body-image-container-text'>Hotel Aryaas</span>
+                  <div className='body-image-container-3'>
+                      <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                      <span className='body-image-container-text-1' >4.5 &#8226; 14mins</span><br></br>
+                  </div>
+                  <span className='body-image-container-text-2' >South Indian<br></br>Pollachi</span>
+              </div>
+              </a>
+          </div>
+        <div className='body-image-container'>
+            <a href='' className='body-image-container-anchor'>
+            <div class="body-image-container-1">
+              <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/f1bc9ddf53de574cdc35ab2f717df234" ></img>
+          
+            </div>
+            <div className='body-image-container-2'>
+              <span className='body-image-container-text'>Hotel Gowri KKrishna</span>
+              <div className='body-image-container-3'>
+                <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                <span className='body-image-container-text-1' >4.3 &#8226; 18mins</span><br></br>
+              </div>
+              <span className='body-image-container-text-2' >South Indian,North Indian,..<br></br>Pollachi</span>
+            </div>
+            </a>
+        </div>
+        <div className='body-image-container'>
+            <a href='' className='body-image-container-anchor'>
+            <div class="body-image-container-1">
+              <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b14cd9fc40129fcfb97aa7e621719d07" ></img>
+              <div class="centered">10% OFF UPTO ₹40</div>
+            </div>
+            <div className='body-image-container-2'>
+              <span className='body-image-container-text'>Madhava Multicusine...</span>
+              <div className='body-image-container-3'>
+                <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                <span className='body-image-container-text-1' >4.6 &#8226; 18mins</span><br></br>
+              </div>
+              <span className='body-image-container-text-2' >South Indian,North Indian,..<br></br>Pollachi</span>
+            </div>
+            </a>
+        </div>
+        <div className='body-image-container'>
+            <a href='' className='body-image-container-anchor'>
+              <div class="body-image-container-1">
+                <img className='body-image-container-pic' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uorbn09yih2szvmzvtql" ></img>
+                <div class="centered">50% OFF UPTO ₹100</div>
+              </div>
+              <div className='body-image-container-2'>
+                <span className='body-image-container-text'>Kwality Walls Frozen Dess..</span>
+                <div className='body-image-container-3'>
+                  <img src='/images/star-inside-circle.png' width={19} height={21}></img> 
+                  <span className='body-image-container-text-1' >4.6 &#8226; 16mins</span><br></br>
+                </div>
+                <span className='body-image-container-text-2' >Desserts, Ice Cream, Ice Cream...<br></br>Pollachi</span>
+              </div>
+            </a>
+        </div>
+      </div>
+      
+
 
 
 
